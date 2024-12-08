611855452f5b669ab5a0c41eb6f9319c7ac8660d
Aric Akbani <aricakbani@gmail.com>
2024-12-07 20:09:12 -0600

    Added a full screen toggle

--- game.gd
+++ game.gd
@@ -5,3 +5,8 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit_game"):
 		get_tree().quit()
 		
+	if event.is_action_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == 0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		elif DisplayServer.window_get_mode() == 3:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
